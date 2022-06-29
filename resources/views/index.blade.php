20732006eee68dd63663e9062704719700596ff7
david <david@vaneffen.be>
2022-06-29 10:20:06 +0200

    ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,30 +5,31 @@
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
 
-
-                <table id="datatable" class="table table-bordered table-striped table-sm dataTable">
-                    <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Status</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($approvers as $approver)
+                @if(!is_null($approvers))
+                    <table id="datatable" class="table table-bordered table-striped table-sm dataTable">
+                        <thead>
                         <tr>
-                            <td>
-                                @if(get_class($approver->approvers) === 'App\Models\ExpenseRequest')
-                                    <a href="{{route('expenserequest.edit', $approver->approvers).'?tab=approvers'}}">{{$approver->approvers->uniqueid}}</a>
-                                @else
-                                    <a href="{{route('rfa.edit', $approver->approvers).'?tab=approvers'}}">{{$approver->approvers->uniqueid}}</a>
-                                @endif
-
-                            </td>
-                            <td>{!! getStatus($approver->status) !!}</td>
+                            <th>Title</th>
+                            <th>Status</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($approvers as $approver)
+                            <tr>
+                                <td>
+                                    @if(get_class($approver->approvers) === 'App\Models\ExpenseRequest')
+                                        <a href="{{route('expenserequest.edit', $approver->approvers).'?tab=approvers'}}">{{$approver->approvers->uniqueid}}</a>
+                                    @else
+                                        <a href="{{route('rfa.edit', $approver->approvers).'?tab=approvers'}}">{{$approver->approvers->uniqueid}}</a>
+                                    @endif
+
+                                </td>
+                                <td>{!! getStatus($approver->status) !!}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @endif
             </div>
         </div>
     </div>
