f52e59bb9d2a43fd10cf1e5b92091f4b269c99d7
David van Effen <david@vaneffen.be>
2022-09-23 11:10:03 +0200

    ok

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('mail:daily')->dailyAt('7:00');
     }
 
     /**
