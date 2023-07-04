7d4774a77dee01a6bce746a4e3cfc98ee8153ef3
arno-ngi <arno@next-gen-ict.be>
2023-07-04 13:37:44 +0200

    added begunstigde translation to lang files law.begunstige

--- resources/views/expenserequests/index.blade.php
+++ resources/views/expenserequests/index.blade.php
@@ -35,7 +35,7 @@
                             <th>{{__('law.amount')}}</th>
                             <th>{{__('law.category')}}</th>
                             <th>Status</th>
-                            <th>{{__('law.begunstigde')}}</th>
+                            <th>{{__('law.beneficiary')}}</th>
                         </tr>
                         </thead>
                         <tbody>
